8ac67d4e2626107d57054a4dc1d756e7be775e22
amarjeetpatidar <amarjeet.patidar4@gmail.com>
2021-12-07 14:34:10 +0530

    Created Models

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/homepage.dart';
 
 
-void main(){
-  runApp(MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
---
+++ lib/models/user_model.dart
@@ -0,0 +1,28 @@
+
+class UserModel{
+  String? uid;
+  String? fullName;
+  String? email;
+  String? profilepic;
+
+  UserModel({this.uid,this.fullName,this.email,this.profilepic});
+
+  UserModel.fromMap(Map<String,dynamic> map){
+    uid = map['uid'];
+    fullName = map['fullName'];
+    email = map['email'];
+    profilepic = map['profilepic'];
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      "uid": uid,
+      "fullName": fullName,
+      "email": email,
+      "profilepic": profilepic
+    };
+  }
+
+
+
+}
