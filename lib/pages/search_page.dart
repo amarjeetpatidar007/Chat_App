fcba41173e87c7d5e1b8bddadb7d87199532575b
amarjeetpatidar <amarjeet.patidar4@gmail.com>
2021-12-14 22:32:04 +0530

    Updated Data Models, Added Chat Functionality

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
 import 'dart:ui';
 
+import 'package:app_provider/main.dart';
+import 'package:app_provider/models/chat_room_model.dart';
 import 'package:app_provider/models/user_model.dart';
 import 'package:app_provider/pages/chat_room_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -11,7 +14,9 @@ class SearchPage extends StatefulWidget {
   final UserModel userModel;
   final User firebaseUser;
 
-  const SearchPage({Key? key, required this.userModel, required this.firebaseUser}) : super(key: key);
+  const SearchPage(
+      {Key? key, required this.userModel, required this.firebaseUser})
+      : super(key: key);
 
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -19,6 +24,44 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   TextEditingController searchController = TextEditingController();
+
+  Future<ChatRoomModel?> getChatRoomModel(UserModel targetUser) async {
+    ChatRoomModel chatRoomModel;
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('chatrooms')
+        .where("participants.${widget.userModel.uid}", isEqualTo: true)
+        .where("participants.${targetUser.uid}", isEqualTo: true)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      //Fetch Existing ChatRoom
+      log("Already Exists");
+      var snapData = querySnapshot.docs[0].data();
+      ChatRoomModel existingChatRoom = ChatRoomModel.fromMap(snapData as Map<String, dynamic>);
+      chatRoomModel = existingChatRoom;
+
+    } else {
+      // Create New ChatRoom
+      log("Create New");
+      ChatRoomModel newChatRoomModel = ChatRoomModel(
+          chatRoomId: uuid.v1(),
+          participants: {
+            widget.userModel.uid.toString(): true,
+            targetUser.uid.toString(): true
+          },
+          lastMessage: "");
+
+      FirebaseFirestore.instance
+          .collection('chatrooms')
+          .doc(newChatRoomModel.chatRoomId.toString())
+          .set(newChatRoomModel.toMap());
+
+      chatRoomModel = newChatRoomModel;
+    }
+
+    return chatRoomModel;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,41 +92,52 @@ class _SearchPageState extends State<SearchPage> {
               StreamBuilder(
                   stream: FirebaseFirestore.instance
                       .collection('users')
-                      .where('email', isEqualTo: searchController.text).where('email', isNotEqualTo: widget.userModel.email)
+                      .where('email', isEqualTo: searchController.text)
+                      .where('email', isNotEqualTo: widget.userModel.email)
                       .snapshots(),
                   builder: (context, snapshot) {
-                    if(snapshot.connectionState == ConnectionState.active){
-                      if(snapshot.hasData){
-                        QuerySnapshot dataSnapshot = snapshot.data as QuerySnapshot;
-                        if(dataSnapshot.docs.isNotEmpty){
-                          Map<String, dynamic> userMap = dataSnapshot.docs[0].data() as Map<String, dynamic>;
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      if (snapshot.hasData) {
+                        QuerySnapshot dataSnapshot =
+                            snapshot.data as QuerySnapshot;
+                        if (dataSnapshot.docs.isNotEmpty) {
+                          Map<String, dynamic> userMap = dataSnapshot.docs[0]
+                              .data() as Map<String, dynamic>;
                           UserModel searchedUser = UserModel.fromMap(userMap);
                           return ListTile(
-                            onTap: (){
-                              Navigator.pop(context);
-                              Navigator.push(context, MaterialPageRoute(builder: (context){
-                                return const ChatRoomPage();
-                              }));
+                            onTap: () async {
+                              ChatRoomModel? chatRoomModel =
+                                  await getChatRoomModel(searchedUser);
+
+                              if(chatRoomModel != null){
+                                Navigator.pop(context);
+                                Navigator.push(context, MaterialPageRoute(builder: (context){
+                                  return ChatRoomPage(
+                                      firebaseUser: widget.firebaseUser,
+                                      userModel: widget.userModel,
+                                      chatRoomModel: chatRoomModel,
+                                      targetUser: searchedUser);
+                                }));
+                              }
+
                             },
                             leading: CircleAvatar(
-                              backgroundImage: NetworkImage(searchedUser.profilepic!),
+                              backgroundImage:
+                                  NetworkImage(searchedUser.profilepic!),
                             ),
                             title: Text(searchedUser.fullName!),
                             subtitle: Text(searchedUser.email!),
                             trailing: const Icon(Icons.keyboard_arrow_right),
                           );
-                        }else{
+                        } else {
                           return const Text('No Users Found!');
                         }
-
-
-                      }else if(snapshot.hasError){
+                      } else if (snapshot.hasError) {
                         return const Text('Error Occurred');
-                      }else{
+                      } else {
                         return const Text('No Result Found!');
                       }
-
-                    }else{
+                    } else {
                       return const CircularProgressIndicator();
                     }
                   })
